9c11dacb0a46d5cd53629097474ae62c82d3b7c2
JuliaZaykaS <sys55@mail.ru>
2021-07-02 15:46:11 +0300

    доделала фильтр в портфолио

--- js/filter.js
+++ js/filter.js
@@ -1,49 +1,48 @@
 const refs = {
-    controlsList: document.querySelector('[data-controls="controls"]'),
-    panelsList: document.querySelector('[data-panels="panels"]'),
-    panel: document.querySelectorAll('[data-panel]'),
-    control: document.querySelectorAll('[data-control]'),
+  controlsList: document.querySelector('[data-controls="controls"]'),
+//   panelsList: document.querySelector('[data-panels="panels"]'),
+  panel: document.querySelectorAll("[data-panel]"),
+  control: document.querySelectorAll("[data-control]"),
 };
 
-console.log(refs.controlsList);
-console.dir(refs.controlsList);
-console.log(refs.panelsList);
-console.dir(refs.panel);
-// console.log(refs.panel.dataset.panel);
+[...refs.panel].forEach((el) => {
+  el.classList.add("active-card");
+});
 
-
-
-refs.controlsList.addEventListener('click', onBtnFilterClick);
+refs.controlsList.addEventListener("click", onBtnFilterClick);
 
 function onBtnFilterClick(e) {
+  if (e.target === e.currentTarget) return;
 
-    if (e.target === e.currentTarget) return;
+  const currentTargetFilterBtn =
+    refs.controlsList.querySelector(".filter-button");
 
-    const currentTargetFilterBtn = refs.controlsList.querySelector('.filter-button');
-    if (currentTargetFilterBtn) {
-        currentTargetFilterBtn.classList.remove('filter-button');
+  if (currentTargetFilterBtn) {
+    currentTargetFilterBtn.classList.remove("filter-button");
+  }
+
+  e.target.classList.add("filter-button");
+
+  [...refs.panel].forEach((el) => {
+    if (el.classList.contains("hidden-card")) {
+      el.classList.remove("hidden-card");
+      el.classList.add("active-card");
     }
+  });
 
-    e.target.classList.add('filter-button');
-   console.log(e.target.dataset.control);
-
-    const panelArray = [...refs.panel].map((el) => {
-    // console.log(el);
-    return el.dataset.panel;
+  if (e.target.dataset.control === "all") {
+    [...refs.panel].forEach((el) => {
+      el.classList.add("active-card");
+      el.classList.remove("hidden-card");
     });
-    const controlArray = [...refs.control].map((el) => {
-        return el.dataset.control;
-    })
-console.log(controlArray);
-   const filteredPanels = panelArray.filter((elem) => {
-       if (e.target.dataset.control === elem) {
-           console.log(elem);
-       }
-       return elem;
-   })
-    console.log(panelArray);
-    console.log(filteredPanels);
-
-
-
+  } else {
+    [...refs.panel]
+      .filter((elem) => {
+        if (e.target.dataset.control !== elem.dataset.panel) return elem;
+      })
+      .forEach((elem) => {
+        elem.classList.add("hidden-card");
+        elem.classList.remove("active-card");
+      });
+  }
 }
